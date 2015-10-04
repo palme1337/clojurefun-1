ece65151805c87343d6e26d3c5e191609fb8c191
wegry <zweg@Zachs-iMac.local>
2015-10-03 23:41:04 -0700

    Sweet second state simulator

--- src/cljs/myproject/core.cljs
+++ src/cljs/myproject/core.cljs
@@ -1,105 +1,94 @@
 (ns myproject.core
-  (:require [reagent.core :as reagent :refer [atom]]
-    [reagent.session :as session]
-    [secretary.core :as secretary :include-macros true]
-    [goog.events :as events]
-    [goog.history.EventType :as EventType])
-  (:import goog.History))
+ (:require [reagent.core :as reagent :refer [atom]]
+  [reagent.session :as session]
+  [secretary.core :as secretary :include-macros true]
+  [goog.events :as events]
+  [goog.history.EventType :as EventType])
+ (:import goog.History))
 
-;; -------------------------
-;; Views
+	;; -------------------------
+	;; Views
 
-(def checkbox-state
-  (reagent/atom 
-  {
-    "Tree Owner desu" true,
-    "Approval params desuuu" true,
-    "Herb please" false
-  })
-  )
+	(def checkbox-state
+	 (reagent/atom 
+	  {
+	  "Tree Owner desu" true,
+	  "Approval params desuuu" true,
+	  "Herb please" false
+	  })
+	)
 
-(defn checkbox [name-to-state]
-  (let [checkbox-name (first name-to-state)
-    checked? (second name-to-state)
-    on-click-handler (fn [] (assoc @checkbox-state checkbox-name (not checked?)))
-    ]
-    [:div {:key name-to-state}
-    [:input {:id checkbox-name,
-      :type "checkbox",
-      :checked checked?,
-      :on-click #(swap! checkbox-state on-click-handler)
-    }
-    ]
-    [:label {:for checkbox-name} checkbox-name]
-    ]
-    )
-  )
-
-
-(defn submit-button [value]
-  [:button {     
-    :disabled (every? false? (vals @checkbox-state))
-    } value
-  ]
-)
-
-(defn home-page []
-  (let [weep (reagent/atom {
-    "Tree Owner desu" false,
-    "Approval params desuuu" true
-  })
-  __ (print "shutup")] 
-    [:div
-     (map checkbox (seq @checkbox-state) )
-     [submit-button "Click me lol"]
-     [:button 
-        "im text in a streing"]
-     ]
-     )
-)
+	(defn checkbox [name-to-state]
+	 (let [checkbox-name (first name-to-state)
+	  checked? (second name-to-state)
+	  on-click-handler (fn [] (assoc @checkbox-state checkbox-name (not checked?)))
+	  ]
+	  [:div {:key name-to-state}
+	  [:input {:id checkbox-name,
+	  :type "checkbox",
+	  :checked checked?,
+	  :on-click #(swap! checkbox-state on-click-handler)
+	  }
+	  ]
+	  [:label {:for checkbox-name} checkbox-name]
+	  ]
+	 )
+	)
 
 
-; (defn home-page []
-;   (let [checkbox-text ["Tree Owner" "Approval Params"]]
-;   [:div [:h2 "Welcome to myproject"]
-;   (map checkbox-names checkbox-text)
-;   [:div [:a {:href "#/about"} "go to about page"]]
-;   [:button {:disabled false} "If you chew Big Red, then fuck you."]]
-; ))
+	(defn submit-button [value]
+	 [:button {     
+	 :disabled (every? false? (vals @checkbox-state))
+	 } value
+	 ]
+	)
 
-; (defn about-page []
-;   [:div [:h2 "About myproject"]
-;   [:div [:a {:href "#/"} "go to the home page"]]])
+	(defn home-page []
+	 (let [new-state (fn [] (merge @checkbox-state {
+				 "Tree Owner" false,
+				 "Approval params" true
+				 }))]
+	  [:div
+    [:h1 "Create a new configuration Request"]
+	  (map checkbox (seq @checkbox-state) )
+	  [submit-button "Click me lol"]
+	  [:button 
+	  {:on-click #(swap! checkbox-state new-state)}
+	  "Test the state here"]
+	  ]
+	 )
+	)
 
-(defn current-page []
-  [:div [(session/get :current-page)]])
 
-;; -------------------------
-;; Routes
-(secretary/set-config! :prefix "#")
+	(defn current-page []
+	 [:div [(session/get :current-page)]])
 
-(secretary/defroute "/" []
-  (session/put! :current-page #'home-page))
+	;; -------------------------
+	;; Routes
+	(secretary/set-config! :prefix "#")
 
-; (secretary/defroute "/about" []
-;   (session/put! :current-page #'about-page))
+	(secretary/defroute "/" []
+	 (session/put! :current-page #'home-page))
 
-;; -------------------------
-;; History
-;; must be called after routes have been defined
-(defn hook-browser-navigation! []
-  (doto (History.)
-    (events/listen
-     EventType/NAVIGATE
-     (fn [event]
-       (secretary/dispatch! (.-token event))))
-    (.setEnabled true)))
+	; (secretary/defroute "/about" []
+			;   (session/put! :current-page #'about-page))
 
-;; -------------------------
-;; Initialize app
-(defn mount-root []
-  (reagent/render [current-page] (.getElementById js/document "app")))
+	;; -------------------------
+	;; History
+	;; must be called after routes have been defined
+	(defn hook-browser-navigation! []
+	 (doto (History.)
+	  (events/listen
+	   EventType/NAVIGATE
+	   (fn [event]
+	    (secretary/dispatch! (.-token event))))
+	  (.setEnabled true)))
 
-(defn init! []
-  (hook-browser-navigation!)
-  (mount-root))
+	;; -------------------------
+	;; Initialize app
+	(defn mount-root []
+	 (reagent/render [current-page] (.getElementById js/document "app")))
+
+	(defn init! []
+	 (hook-browser-navigation!)
+	 (mount-root))
